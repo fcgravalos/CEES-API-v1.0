648422da3edcb8d1e0e2f128d05f54cdbf4fb125
unknown <fcrespo@MOB11467.hq.k.grp>
2014-12-23 20:00:22 +0100

    CLEANING UP THE REST_API REPO

--- background.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': Math.round(window.screen.availWidth),
-      'height': Math.round(window.screen.availHeight)
-    }
-  });
-});
